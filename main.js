d5970c8cf1db5d2df0f6c7564e0ec253d4aba728
Andre Silva <undreadbr@yahoo.com.br>
2023-06-21 12:02:48 -0300

    limitação casas decimais

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function atualizaTabela(){
 
 function atualizaMediaFinal(){
     mediaFinal = calculaMediaFinal();
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado :spanReprovado;
 }
 
